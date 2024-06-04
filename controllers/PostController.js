b19f10c41f93c1c44f75db9f6ab698ef6a915011
An4ikhun733r <anuar.ospanov.prof@gmail.com>
2024-06-04 11:45:38 +0500

    added expiration post feature

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -2,13 +2,19 @@ import PostModel from "../models/Post.js";
 
 export const create = async (req, res) => {
   try {
+    const { content, expiresIn } = req.body;
+    const expiresAt = new Date(Date.now() + expiresIn * 1000);
+    const expiresAtLocal = new Date(
+      expiresAt.toLocaleString("en-US", { timeZone: "Asia/Almaty" })
+    );
+
     const doc = new PostModel({
       title: req.body.title,
       text: req.body.text,
       imageUrl: req.body.imageUrl,
       tags: req.body.tags,
       user: req.userId,
-      shortUrl: req.shortUrl,
+      expiresAt: expiresAt,
     });
 
     const post = await doc.save();
@@ -37,16 +43,25 @@ export const getAll = async (req, res) => {
 export const getOne = async (req, res) => {
   try {
     const url = req.params.shortUrl;
-    await PostModel.findOneAndUpdate(
+    const post = await PostModel.findOneAndUpdate(
       {
         shortUrl: url,
       },
       {
         $inc: { viewsCount: 1 },
-      }
+      },
+      { new: true }
     );
 
-    const post = await PostModel.findOne({ shortUrl: url }).populate("user");
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+
+    if (new Date() > post.expiresAt) {
+      await PostModel.deleteOne({shortUrl: url});
+      return res.status(410).json({ error: "Post expired" });
+    }
+
     res.json(post);
   } catch (err) {
     console.log(err);
@@ -74,7 +89,7 @@ export const remove = async (req, res) => {
 export const update = async (req, res) => {
   try {
     const postId = req.params.id;
-    
+
     await PostModel.updateOne(
       { _id: postId },
       {
--- models/Post.js
+++ models/Post.js
@@ -1,35 +1,38 @@
 import mongoose from "mongoose";
 import shortid from "shortid";
 
-const PostSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-    },  
-    tags: { type: Array, default: [] },
-    text: {
-      type: String,
-      required: true,
-    },
-    imageUrl: String,
-    viewsCount: {
-      type: Number,
-      default: 0,
-    },
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    shortUrl: {
-      type: String,
-      default: shortid.generate,
-    }
+const PostSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
   },
-  {
-    timestamps: true,
-  }
-);
+  tags: { type: Array, default: [] },
+  text: {
+    type: String,
+    required: true,
+  },
+  imageUrl: String,
+  viewsCount: {
+    type: Number,
+    default: 0,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  shortUrl: {
+    type: String,
+    default: shortid.generate,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  expiresAt: {
+    type: Date,
+    required: true,
+  },
+});
 
 export default mongoose.model("Post", PostSchema);
